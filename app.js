21afc7a21d3c2a1a6933bb166c35c27f3a842cae
Elnatal Debebe <elnataldebebe@gmail.com>
2020-10-26 21:55:02 +0300

    route setup for employees

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use('/setting', require('./routes/settings'));
 app.use('/tickets', require('./routes/tickets'));
 app.use('/notifications', require('./routes/notifications'));
 app.use('/corporates', require('./routes/corporates'));
+app.use('/employees', require('./routes/employees'));
 app.use('/corporate-payments', require('./routes/corporatePayments'));
 app.use('/trips', require('./routes/trips'));
 app.use('/rents', require('./routes/rents'));
