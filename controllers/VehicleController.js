3b99123f625e2ca77a141178fc45389734b264ca
elnatal <elnataldebebe@gmail.com>
2021-02-10 12:04:06 +0300

    :ambulance:

--- controllers/VehicleController.js
+++ controllers/VehicleController.js
@@ -48,7 +48,7 @@ const index =  async (req, res) => {
 
         if (req.query.date != null && req.query.date) {
             var endDate = new Date(req.query.date);
-            endDate.getHours().setHours(24);
+            endDate.setHours(24);
 
             filter['createdAt'] = { $gte: new Date(req.query.date)};
             filter['createdAt'] = { $lte: endDate};
